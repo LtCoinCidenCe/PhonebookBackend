a90a722385c001a108d5c41788b1fb6149ce1d49
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-09-07 15:42:25 +0300

    exercise 3.17 & 18

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ morgan.token('body', function (req, res)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
 app.get('/api/persons', (request, response) =>
 {
   Person.find({}).then(people =>
@@ -55,6 +32,21 @@ app.get('/api/persons/:id', (request, response, next) =>
     else
       response.status(404).end();
   })
+    .catch(error => next(error));
+})
+
+app.put('/api/persons/:id', (request, response, next) =>
+{
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson =>
+    {
+      response.json(updatedPerson);
+    })
     .catch(error => next(error));
 })
 
